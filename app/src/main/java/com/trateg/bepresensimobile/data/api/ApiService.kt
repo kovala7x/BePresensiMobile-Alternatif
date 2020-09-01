0474684794371fa6661f8675deaa3aa30a4162e8
Ilham Cahyahadi Pamungkas <ilham.cahyahadi.tif17@polban.ac.id>
2020-09-01 22:14:05 +0700

    Perbaikan Header API

--- app/src/main/java/com/trateg/bepresensimobile/data/api/ApiService.kt
+++ app/src/main/java/com/trateg/bepresensimobile/data/api/ApiService.kt
@@ -1,14 +1,12 @@
 package com.trateg.bepresensimobile.data.api
 
 import com.trateg.bepresensimobile.model.BaseResponse
-import kotlinx.coroutines.Deferred
-import retrofit2.Call
-import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
 
 interface ApiService {
-    @Headers({"Accept: application/json"})
+    @Headers("Accept: application/json")
     @GET("jadwal/mahasiswa/{nim}")
     suspend fun getJadwal(@Path("nim") nim :String) : BaseResponse
 }
